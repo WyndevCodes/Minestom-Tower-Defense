963e68f8e4988ca33563ca0c9bd287e0f62837a0
____tbvns____ <tbvns601@gmail.com>
2024-07-24 06:32:11 +0200

    Player can now log in + added a stone block at (0 -5 0)

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,6 @@ dependencies {
     implementation("org.slf4j:slf4j-api:2.0.13")
     implementation("ch.qos.logback:logback-classic:1.5.6")
     implementation("net.minestom:minestom-snapshots:1f34e60ea6")
-
 }
 
 tasks.test {
