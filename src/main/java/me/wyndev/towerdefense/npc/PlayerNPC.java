6721e89c82c9c8736099c149d6846fbda909f940
Wyndev <108705186+WyndevCodes@users.noreply.github.com>
2024-07-27 22:20:57 -0500

    NPC no longer has collisions, npc looks at nearest player (max radius 7)

--- src/main/java/me/wyndev/towerdefense/npc/PlayerNPC.java
+++ src/main/java/me/wyndev/towerdefense/npc/PlayerNPC.java
@@ -1,14 +1,19 @@
 package me.wyndev.towerdefense.npc;
 
+import net.kyori.adventure.text.Component;
+import net.minestom.server.MinecraftServer;
 import net.minestom.server.entity.*;
 import net.minestom.server.network.packet.server.play.EntityMetaDataPacket;
 import net.minestom.server.network.packet.server.play.PlayerInfoRemovePacket;
 import net.minestom.server.network.packet.server.play.PlayerInfoUpdatePacket;
+import net.minestom.server.network.packet.server.play.TeamsPacket;
+import net.minestom.server.scoreboard.Team;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.Optional;
 
 /**
  * A player-like entity with a player skin.
@@ -20,9 +25,14 @@ import java.util.Map;
 @SuppressWarnings("UnstableApiUsage")
 public abstract class PlayerNPC extends LivingEntity {
     private final String username;
+    private final Component nametag;
 
     private final String skinTexture;
     private final String skinSignature;
+
+    private Team npcTeam;
+
+    private Entity target;
 
     /**
      * Creates a player NPC.
@@ -32,12 +42,45 @@ public abstract class PlayerNPC extends LivingEntity {
      */
     public PlayerNPC(@NotNull String username, @Nullable String skinTexture, @Nullable String skinSignature) {
         super(EntityType.PLAYER);
-        this.username = username;
+        this.username = "$" + username;
+        this.nametag = null;
 
         this.skinTexture = skinTexture;
         this.skinSignature = skinSignature;
 
         setNoGravity(true);
+        createNPCTeam();
+        createNametag();
+    }
+
+    /**
+     * Creates a player NPC.
+     * @param username The username of the player NPC
+     * @param nametag The NPC nametag, in component format
+     * @param skinTexture The skin texture of the player, may be null
+     * @param skinSignature The skin signature of the player, may be null
+     */
+    public PlayerNPC(@NotNull String username, @Nullable Component nametag, @Nullable String skinTexture, @Nullable String skinSignature) {
+        super(EntityType.PLAYER);
+        this.username = "$" + username;
+        this.nametag = nametag;
+
+        this.skinTexture = skinTexture;
+        this.skinSignature = skinSignature;
+
+        setNoGravity(true);
+        createNPCTeam();
+        createNametag();
+    }
+
+    private void createNPCTeam() {
+        npcTeam = MinecraftServer.getTeamManager().createBuilder("npc_team_" + username)
+                .collisionRule(TeamsPacket.CollisionRule.NEVER).nameTagVisibility(TeamsPacket.NameTagVisibility.NEVER).build();
+        npcTeam.addMember(username);
+    }
+
+    private void createNametag() {
+        //TODO: visual nameplate display, maybe multiline?
     }
 
     /**
@@ -45,6 +88,25 @@ public abstract class PlayerNPC extends LivingEntity {
      * @param player The player who clicked
      */
     public abstract void onClick(Player player);
+
+    @Override
+    public void update(long time) {
+        super.update(time);
+
+        if (time % 20 == 0) {
+            Optional<Entity> nearest = instance.getNearbyEntities(position, 7).stream()
+                    .filter(e -> e instanceof Player)
+                    .min((a, b) -> (int) Math.round(a.getDistanceSquared(b)));
+            nearest.ifPresent(entity -> target = entity);
+        }
+        if (target != null && target.getInstance().equals(instance)) lookAt(target);
+    }
+
+    @Override
+    public void remove() {
+        MinecraftServer.getTeamManager().deleteTeam(npcTeam);
+        super.remove();
+    }
 
     @Override
     public void updateNewViewer(@NotNull Player player) {
